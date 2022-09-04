d4f1362f168addb18bb4094ac29efe7b1116528e
Reveillark <vlt31@hotmail.fr>
2022-09-04 19:27:18 +0200

    Separateded dirt mod

---
+++ mods/CORE/cw_dirt/nodes.lua
@@ -0,0 +1,40 @@
+
+
+minetest.register_node("cw_core:dirt_with_grass", {
+	description = "Dirt with Grass",
+	--tiles ={{name='cw_grass_256x256.png^(cw_rngmask_48x48.png^[resize:256x256^[opacity:16)',align_style='world', scale=16},
+	--tiles ={{name='cw_grass_256x256.png^[mask:cw_rngmask_48x48.png',align_style='world', scale=16},
+	tiles ={{name='cw_grass_256x256.png',align_style='world', scale=16},
+		-- a little dot on the bottom to distinguish it from dirt
+		"mc_dirt.png^basenodes_dirt_with_grass_bottom.png",
+		{name = "mc_dirt.png^default_grass_side.png",
+		tileable_vertical = false,}},
+	groups = {crumbly=3, soil=1, oddly_breakable_by_hand=1},
+})
+
+minetest.register_node("cw_core:dirt_with_snow", {
+	description = "Dirt with Snow",
+	tiles ={"basenodes_dirt_with_snow.png",
+		-- a little dot on the bottom to distinguish it from dirt
+		"default_dirt.png^basenodes_dirt_with_snow_bottom.png",
+		{name = "mc_dirt.png^default_snow_side.png",
+		tileable_vertical = false}},
+	groups = {crumbly=3, soil=1},
+})
+
+minetest.register_node("cw_core:dirt", {
+	description = "Dirt",
+	tiles ={"mc_dirt.png"},
+	groups = {crumbly=3, soil=1},
+})
+
+minetest.register_node("cw_core:junglegrass", {
+	description = "Jungle Grass",
+	drawtype = "plantlike",
+	tiles ={"default_junglegrass.png"},
+	inventory_image = "default_junglegrass.png",
+	wield_image = "default_junglegrass.png",
+	paramtype = "light",
+	walkable = false,
+	groups = {snappy=3},
+})
--- mods/cw_core/nodes.lua
+++ mods/cw_core/nodes.lua
@@ -2,46 +2,10 @@ local WATER_ALPHA = "^[opacity:" .. 160
 local WATER_VISC = 1
 local LAVA_VISC = 7
 
-
---minetest.register_node("cw_core:stone", {
---	description = "Stone",
-	--tiles = {"default_stone.png"},
---	tiles ={{name='cw_stone_256x256.png',align_style='world', scale=16}},
---	groups = {cracky=3},
---})
-
 minetest.register_node("cw_core:desert_stone", {
 	description = "Desert Stone",
 	tiles = {"default_desert_stone.png"},
 	groups = {cracky=3},
-})
-
-minetest.register_node("cw_core:dirt_with_grass", {
-	description = "Dirt with Grass",
-	--tiles ={{name='cw_grass_256x256.png^(cw_rngmask_48x48.png^[resize:256x256^[opacity:16)',align_style='world', scale=16},
-	--tiles ={{name='cw_grass_256x256.png^[mask:cw_rngmask_48x48.png',align_style='world', scale=16},
-	tiles ={{name='cw_grass_256x256.png',align_style='world', scale=16},
-		-- a little dot on the bottom to distinguish it from dirt
-		"mc_dirt.png^basenodes_dirt_with_grass_bottom.png",
-		{name = "mc_dirt.png^default_grass_side.png",
-		tileable_vertical = false,}},
-	groups = {crumbly=3, soil=1, oddly_breakable_by_hand=1},
-})
-
-minetest.register_node("cw_core:dirt_with_snow", {
-	description = "Dirt with Snow",
-	tiles ={"basenodes_dirt_with_snow.png",
-		-- a little dot on the bottom to distinguish it from dirt
-		"default_dirt.png^basenodes_dirt_with_snow_bottom.png",
-		{name = "mc_dirt.png^default_snow_side.png",
-		tileable_vertical = false}},
-	groups = {crumbly=3, soil=1},
-})
-
-minetest.register_node("cw_core:dirt", {
-	description = "Dirt",
-	tiles ={"mc_dirt.png"},
-	groups = {crumbly=3, soil=1},
 })
 
 minetest.register_node("cw_core:sand", {
@@ -60,17 +24,6 @@ minetest.register_node("cw_core:gravel", {
 	description = "Gravel",
 	tiles ={"default_gravel.png"},
 	groups = {crumbly=2},
-})
-
-minetest.register_node("cw_core:junglegrass", {
-	description = "Jungle Grass",
-	drawtype = "plantlike",
-	tiles ={"default_junglegrass.png"},
-	inventory_image = "default_junglegrass.png",
-	wield_image = "default_junglegrass.png",
-	paramtype = "light",
-	walkable = false,
-	groups = {snappy=3},
 })
 
 minetest.register_node("cw_core:tree", {
