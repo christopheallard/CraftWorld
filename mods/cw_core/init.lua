36737f206d73f9dfa5f544e830dba3dc24a248aa
christopheallard <41883407+christopheallard@users.noreply.github.com>
2022-09-04 17:48:41 +0200

    Test rammassage main

--- mods/cw_core/init.lua
+++ mods/cw_core/init.lua
@@ -36,7 +36,7 @@ minetest.register_node("cw_core:dirt_with_grass", {
 		"mc_dirt.png^basenodes_dirt_with_grass_bottom.png",
 		{name = "mc_dirt.png^default_grass_side.png",
 		tileable_vertical = false,}},
-	groups = {crumbly=3, soil=1},
+	groups = {crumbly=3, soil=1, oddly_breakable_by_hand=1},
 })
 
 minetest.register_node("cw_core:dirt_with_snow", {
@@ -358,3 +358,21 @@ minetest.register_node("cw_core:snowblock", {
 })
 
 
+
+
+minetest.register_item(":", {
+	type = "none",
+	wield_image = "wieldhand.png",
+	wield_scale = {x = 1, y = 1, z = 2.5},
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {
+			crumbly = {times = {[2] = 3.00, [3] = 0.70}, uses = 0, maxlevel = 1},
+			snappy = {times = {[3] = 0.40}, uses = 0, maxlevel = 1},
+			oddly_breakable_by_hand =
+				{times = {[1] = 3.50, [2] = 2.00, [3] = 0.70}, uses = 0}
+		},
+		damage_groups = {fleshy = 1},
+	}
+})
