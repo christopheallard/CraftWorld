636c01026390dacc7f571b0ce32a526e44ac4c11
christopheallard <41883407+christopheallard@users.noreply.github.com>
2022-12-30 18:10:18 +0100

    Test d'ajout des gisement et des arbres

--- mods/MAPGEN/cw_mapgen/init.lua
+++ mods/MAPGEN/cw_mapgen/init.lua
@@ -47,6 +47,8 @@ if minetest.settings:get_bool("devtest_dungeon_stairs", false) then
 	end
 end
 
+
+
 --
 -- Register biomes for biome API
 --
@@ -56,7 +58,7 @@ minetest.clear_registered_decorations()
 
 if minetest.settings:get_bool("devtest_register_biomes", true) then
 	minetest.register_biome({
-		name = "mapgen:grassland",
+		name = "cw_mapgen:grassland",
 		node_top = "cw_dirt:dirt_with_grass",
 		depth_top = 1,
 		node_filler = "cw_stone:stone",
@@ -66,7 +68,7 @@ if minetest.settings:get_bool("devtest_register_biomes", true) then
 		node_dungeon = "cw_stone:cobble",
 		node_dungeon_alt = "cw_stone:mossycobble",
 		node_dungeon_stair = "stairs:stair_cobble",
-		y_max = 31000,
+		y_max = 60,
 		y_min = 4,
 		heat_point = 50,
 		humidity_point = 50,
@@ -85,7 +87,7 @@ if minetest.settings:get_bool("devtest_register_biomes", true) then
 		node_dungeon_alt = "cw_stone:mossycobble",
 		node_dungeon_stair = "stairs:stair_cobble",
 		y_max = 3,
-		y_min = -255,
+		y_min = -100,
 		heat_point = 50,
 		humidity_point = 50,
 	})
@@ -96,10 +98,111 @@ if minetest.settings:get_bool("devtest_register_biomes", true) then
 		node_dungeon = "cw_stone:cobble",
 		node_dungeon_alt = "cw_stone:mossycobble",
 		node_dungeon_stair = "stairs:stair_cobble",
-		y_max = -256,
-		y_min = -31000,
+		y_max = -100,
+		y_min = -300,
 		heat_point = 50,
 		humidity_point = 50,
 	})
+
+	minetest.register_biome({
+		name = "mapgen:mountain",
+		node_top = "cw_stone:stone",
+		depth_top = 1,
+		node_filler = "cw_stone:stone",
+		depth_filler = 1,
+		node_riverbed = "cw_stone:stone",
+		depth_riverbed = 2,
+		node_dungeon = "cw_stone:cobble",
+		node_dungeon_alt = "cw_stone:mossycobble",
+		node_dungeon_stair = "stairs:stair_cobble",
+		y_max = 300,
+		y_min = 61,
+		heat_point = 50,
+		humidity_point = 50,
+	})
+
+
+
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "cw_copper:copper_ore",
+		wherein        = "cw_stone:stone",
+		clust_scarcity = 8 * 8 * 8,
+		clust_num_ores = 16,
+		clust_size     = 5,
+		y_min          = 100,
+		y_max          = 200,
+	})
+
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "cw_copper:copper_ore",
+		wherein        = "cw_stone:stone",
+		clust_scarcity = 8 * 8 * 8,
+		clust_num_ores = 8,
+		clust_size     = 3,
+		y_min          = -100,
+		y_max          = 100,
+	})
+
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "cw_copper:copper_ore",
+		wherein        = "cw_stone:stone",
+		clust_scarcity = 10 * 10 * 10,
+		clust_num_ores = 8,
+		clust_size     = 3,
+		y_min          = -100,
+		y_max          = 100,
+	})
+
+
+
+
+	local my_schematic = {
+		size = {x = 3, y = 6, z = 3},
+		data = {
+			-- The side of the bush, with the air on top
+			{name = "air"}, {name = "air"}, {name = "air"},
+			{name = "air"}, {name = "air"}, {name = "air"},
+			{name = "air"}, {name = "air"}, {name = "air"},
+			{name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, -- lower layer
+			{name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, -- middle layer
+			{name = "air"}, {name = "air"}, {name = "air"}, -- top layer
+			
+			-- The center of the bush, with stem at the base and a pointy leave 2 nodes above
+			{name = "air"}, {name = "cw_tree:tree"}, {name = "air"},
+			{name = "air"}, {name = "cw_tree:tree"}, {name = "air"},
+			{name = "air"}, {name = "cw_tree:tree"}, {name = "air"},
+			{name = "cw_tree:leaves"}, {name = "cw_tree:tree"}, {name = "cw_tree:leaves"}, -- lower layer
+			{name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, -- middle layer
+			{name = "air"}, {name = "cw_tree:leaves"}, {name = "air"}, -- top layer
+			
+			-- The other side of the bush, same as first side
+			{name = "air"}, {name = "air"}, {name = "air"},
+			{name = "air"}, {name = "air"}, {name = "air"},
+			{name = "air"}, {name = "air"}, {name = "air"},
+			{name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, -- lower layer
+			{name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, {name = "cw_tree:leaves"}, -- middle layer
+			{name = "air"}, {name = "air"}, {name = "air"}, -- top layer
+		}
+	}
+
+	minetest.register_decoration({
+		name = "mapgen:treetest",
+		deco_type = "schematic",
+		place_on = {"cw_dirt:dirt_with_grass"},
+		sidelen = 6,
+		fill_ratio = 0.02,
+		biomes = {"mapgen:grassland"},
+--		y_max = 3,
+--		y_min = 60,
+--		decoration = "default:tree",
+		schematic = my_schematic,
+		flags = "place_center_x, place_center_z, all_floors",
+--		flags = "force_placement",
+		rotation = "random",
+	})
+
 end
 
